c17c312513359c20189f426d82104eada23a5cc8
kimurareo <k024g0034@g.neec.ac.jp>
2025-10-09 11:28:56 +0900

    kasann gousai

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -19,6 +19,6 @@ PixelShaderOutput main(VertexShaderOutput input)
 {
     PixelShaderOutput output;
     float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
-    output.color = gMaterial.color * textureColor;
+    output.color = gMaterial.color * textureColor ;
     return output;
 }
